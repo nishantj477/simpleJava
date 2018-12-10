d01baeaa967884f28e3e7922acc585300df896ac
nishantj477 <nishantj477@gmail.com>
2018-12-10 12:55:52 +0530

    Changes to main

--- src/org/nishant/Main.java
+++ src/org/nishant/Main.java
@@ -1,9 +1,16 @@
 package org.nishant;
 
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
 	// write your code here
-        Student s1 = new Student("Nishant", "Joshi", 24);
-        System.out.println(s1.getFullName());
+        ArrayList<Student> students = new ArrayList<Student>();
+        students.add(new Student("Nishant", "Joshi", 24));
+        students.add(new Student("Rajesh", "Kumar", 28));
+
+        for(Student s : students) {
+            System.out.println(s.getFullName());
+        }
     }
 }
